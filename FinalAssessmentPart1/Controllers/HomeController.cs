255780c5bbaeb3f8208a86d368859b61fed0e9df
Jacob Hands <jrhands@gmail.com>
2018-09-04 15:50:16 -0400

    Final Assessment Part 2 Problem 1

---
+++ FinalAssessmentPart1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalAssessmentPart1.Models;
+
+namespace FinalAssessmentPart1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View(new Calculator());
+        }
+
+        [HttpPost]
+        public ActionResult About(Calculator model)
+        {
+            if (model.FirstNum >= 1 && model.FirstNum <= 1000 && model.SecondNum >= 1 && model.SecondNum <= 1000)
+            {
+                return RedirectToAction("Result", model);
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Result(Calculator model)
+        {
+            ViewBag.Message = $"{model.FirstNum + model.SecondNum}";
+            return View();
+        }
+    }
+}
---
+++ FinalAssessmentPart1/Models/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FinalAssessmentPart1.Models
+{
+    public class Calculator
+    {
+        [Required]
+        [Display(Name = "First Number")]
+        [Range(1, 1000)]
+        public double FirstNum { get; set; }
+
+        [Required]
+        [Display(Name = "Second Number")]
+        [Range(1, 1000)]
+        public double SecondNum { get; set; }
+    }
+}
